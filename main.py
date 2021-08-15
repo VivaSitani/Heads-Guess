d8c50bcf7d680522836a08dd16be835c9f85f7ea
VivaSitani <66580922+VivaSitani@users.noreply.github.com>
2021-08-14 21:53:57 -0400

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
-text_list: List[str] = []
-text_list = ["DOG", "CLOCK", "BABY", "CAT", "HOT", "FLASHLIGHT", "COLD", "SURFER", "SWIMMER", "STUDENT", "DANCER"]
-
-basic.show_string("A=30 Sec B=60 Sec")
-
 def on_button_pressed_a():
     game.start_countdown(30000)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def on_button_pressed_b():
-    game.start_countdown(60000)
-input.on_button_pressed(Button.B, on_button_pressed_b)
-
-global index
-
 def on_gesture_logo_up():
-    index = randint(0, len(text_list) -1)
-    basic.show_string(text_list[index])
+    global index
+    index = randint(0, len(text_list) - 1)
+    basic.show_string("" + (text_list[index]))
 input.on_gesture(Gesture.LOGO_UP, on_gesture_logo_up)
 
 def on_gesture_screen_down():
     game.add_score(1)
 input.on_gesture(Gesture.SCREEN_DOWN, on_gesture_screen_down)
+
+def on_button_pressed_b():
+    game.start_countdown(60000)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+index = 0
+text_list: List[str] = []
+text_list = ["DOG",
+    "CLOCK",
+    "BABY",
+    "CAT",
+    "HOT",
+    "FLASHLIGHT",
+    "COLD",
+    "SURFER",
+    "SWIMMER",
+    "STUDENT",
+    "DANCER"]
+basic.show_string("A=30 Sec B=60 Sec")
--- main.ts
+++ main.ts
@@ -1,17 +1,29 @@
-let text_list : string[] = []
-text_list = ["DOG", "CLOCK", "BABY", "CAT", "HOT", "FLASHLIGHT", "COLD", "SURFER", "SWIMMER", "STUDENT", "DANCER"]
-basic.showString("A=30 Sec B=60 Sec")
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
+input.onButtonPressed(Button.A, function () {
     game.startCountdown(30000)
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    game.startCountdown(60000)
+input.onGesture(Gesture.LogoUp, function () {
+    index = randint(0, text_list.length - 1)
+    basic.showString("" + (text_list[index]))
 })
-
-input.onGesture(Gesture.LogoUp, function on_gesture_logo_up() {
-    let index = randint(0, text_list.length - 1)
-    basic.showString(text_list[index])
-})
-input.onGesture(Gesture.ScreenDown, function on_gesture_screen_down() {
+input.onGesture(Gesture.ScreenDown, function () {
     game.addScore(1)
 })
+input.onButtonPressed(Button.B, function () {
+    game.startCountdown(60000)
+})
+let index = 0
+let text_list: string[] = []
+text_list = [
+"DOG",
+"CLOCK",
+"BABY",
+"CAT",
+"HOT",
+"FLASHLIGHT",
+"COLD",
+"SURFER",
+"SWIMMER",
+"STUDENT",
+"DANCER"
+]
+basic.showString("A=30 Sec B=60 Sec")
